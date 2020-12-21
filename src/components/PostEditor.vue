850b2198b3fb7d3fad40860db60af08874a5cb69
Dillon Parchment <dillonparchmentkool@gmail.com>
2020-12-21 09:19:19 -0500

    communicating between components via (emmit and props *Post Edit component*)

---
+++ src/components/PostEditor.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <form @submit.prevent="save">
+      <div class="form-group">
+        <textarea
+          name=""
+          id=""
+          cols="30"
+          rows="10"
+          class="form-input"
+          v-model="newPostText"
+        ></textarea>
+      </div>
+      <div class="form-actions">
+        <button class="btn-blue">Submit Post</button>
+      </div>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    threadId: {
+      required: true
+    }
+  },
+  data () {
+    return {
+      newPostText: ''
+    }
+  },
+  methods: {
+    save () {
+      const postId = 'newPost' + Math.random()
+      const post = {
+        text: this.newPostText,
+        publishedAt: Math.floor(Date.now() / 1000),
+        threadId: this.threadId,
+        userId: 'FsCDAk9w8NeXEceLV87arpsXjnQ2',
+        '.key': postId
+      }
+      this.newPostText = ''
+
+      this.$emit('save-post', {post})
+    }
+  }
+}
+</script>
+<style  scoped>
+</style>
--- src/views/ThreadShowPage.vue
+++ src/views/ThreadShowPage.vue
@@ -3,31 +3,22 @@
     <div class="col-large push-top">
       <h1>{{ thread.title }}</h1>
       <PostList :posts="posts"/>
-      <form @submit.prevent="addPost">
-        <div class="form-group">
-          <textarea 
-          name="" 
-          id="" 
-          cols="30"
-          rows="10" 
-          class="form-input"
-          v-model="newPostText"
-          ></textarea>
-        </div>
-        <div class="form-actions">
-          <button class="btn-blue" >Submit Post</button>
-        </div>
-      </form>
+      <PostEditor 
+        @save-post="addPost"
+        :threadId ="id"
+      />
     </div>
   </div>
 </template>
 <script>
 import sourceData from '@/data'
 import PostList from '@/components/PostList'
+import PostEditor from '@/components/PostEditor'
 export default {
   name: 'ThreadShowPage',
   components: {
-    PostList
+    PostList,
+    PostEditor
   },
   props: {
     id: {
@@ -37,8 +28,7 @@ export default {
   },
   data () {
     return {
-      thread: sourceData.threads[this.id],
-      newPostText: ''
+      thread: sourceData.threads[this.id]
     }
   },
   computed: {
@@ -49,19 +39,12 @@ export default {
     }
   },
   methods: {
-    addPost () {
-      const postId = 'newPost' + Math.random()
-      const post = {
-        text: this.newPostText,
-        publishedAt: Math.floor(Date.now() / 1000),
-        threadId: this.id,
-        userId: 'FsCDAk9w8NeXEceLV87arpsXjnQ2',
-        '.key': postId
-      }
+    addPost (eventData) {
+      const post = eventData.post
+      const postId = eventData.post['.key']
       this.$set(sourceData.posts, postId, post)
       this.$set(this.thread.posts, postId, postId)
       this.$set(sourceData.users[post.userId].posts, postId, postId)
-      this.newPostText = ''
     }
   }
 }
