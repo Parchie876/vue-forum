ee97ef18a4a8a6b2e7b3ca8197e663818ed5e440
Dillon Parchment <dillonparchmentkool@gmail.com>
2021-02-09 16:26:21 -0500

    Creating Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <div id="app">
+    <TheNavbar/>
     <div class="container">
-      <router-view/>
+      <router-view />
     </div>
   </div>
 </template>
 
 <script>
+import TheNavbar from '@/components/TheNavbar'
 export default {
-  name: 'app'
+  components: {
+    TheNavbar
+  }
 }
 </script>
 <style>
