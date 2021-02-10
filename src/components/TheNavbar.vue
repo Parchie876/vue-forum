6a50a4fbc88fcc7b22ab784138fe2ee9e01918ff
Dillon Parchment <dillonparchmentkool@gmail.com>
2021-02-09 23:34:03 -0500

    Creating User Profile Page.

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -16,7 +16,7 @@
       <nav class="navbar">
         <ul>
           <li class="navbar-user">
-            <a href="#">
+            <router-link :to="{name: 'Profile'}">
               <img
                 class="avatar-small"
                 :src="user.avatar"
@@ -30,7 +30,7 @@
                   alt=""
                 />
               </span>
-            </a>
+            </router-link>
 
             <!-- dropdown menu -->
             <!-- add class "active-drop" to show the dropdown -->
--- src/router/index.js
+++ src/router/index.js
@@ -40,6 +40,15 @@ export default new Router({
       props: true
     },
     {
+      path: '/me',
+      name: 'Profile',
+      // route level code-splitting
+      // this generates a separate chunk (profile.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "profile" */ '@/views/ProfilePage'),
+      props: true
+    },
+    {
       path: '*',
       name: 'NotFound',
       // route level code-splitting
