c15d9df26ec22d32d8b06bba1c9bb1e8d99a9697
Dillon Parchment <dillonparchmentkool@gmail.com>
2021-02-15 22:01:17 -0500

    Actions and Mutations for Create New Thread

--- src/store/index.js
+++ src/store/index.js
@@ -26,6 +26,31 @@ export default new Vuex.Store({
       commit('appendPostToUser', {userId: post.userId, postId})
     },
 
+    createThread ({state, commit, dispatch}, {text, title, forumId}) {
+      const threadId = 'newThreadId' + Math.random()
+      const userId = state.authId
+      const publishedAt = Math.floor(Date.now() / 1000)
+      const thread = {
+      //  TODO Add contributors
+      // TODO Add first post ID
+      // TODO Add first post at
+      //  TODO Add Last post at
+      //  TODO Add Last post ID
+      //  TODO Add slug
+      //  TODO Add posts
+        '.key': threadId,
+        forumId,
+        title,
+        publishedAt,
+        userId
+      }
+      commit('setThread', { threadId, thread })
+      commit('appendThreadToForum', {forumId, threadId})
+      commit('appendThreadToUser', {userId, threadId})
+
+      dispatch('createPost', {text, threadId})
+    },
+
     updateUser ({commit}, user) {
       commit('setUser', {userId: user['.key'], user})
     }
@@ -39,15 +64,40 @@ export default new Vuex.Store({
     setUser (state, { user, userId }) {
       Vue.set(state.users, userId, user)
     },
+    setThread (state, { thread, threadId }) {
+      Vue.set(state.threads, threadId, thread)
+    },
 
     appendPostToThread (state, { threadId, postId }) {
       const thread = state.threads[threadId]
+      if (!thread.post) {
+        Vue.set(thread, 'post', {})
+      }
       Vue.set(thread.posts, postId, postId)
     },
 
     appendPostToUser (state, { postId, userId }) {
       const user = state.users[userId]
+      if (!user.post) {
+        Vue.set(user, 'post', {})
+      }
       Vue.set(user.posts, postId, postId)
+    },
+
+    appendThreadToForum (state, {forumId, threadId}) {
+      const forum = state.forums[forumId]
+      if (!forum.post) {
+        Vue.set(forum, 'post', {})
+      }
+      Vue.set(forum.threads, threadId, threadId)
+    },
+
+    appendThreadToUser (state, {userId, threadId}) {
+      const user = state.users[userId]
+      if (!user.post) {
+        Vue.set(user, 'post', {})
+      }
+      Vue.set(user.threads, threadId, threadId)
     }
   }
 })
--- src/views/CreateThreadPage.vue
+++ src/views/CreateThreadPage.vue
@@ -17,7 +17,7 @@
               <div class="form-group">
                 <label for="thread_content">Content:</label>
                 <textarea 
-                    v-model="content" 
+                    v-model="text" 
                     id="thread_content" 
                     class="form-input" 
                     name="content" 
@@ -44,13 +44,17 @@ export default {
     data () {
         return {
         title: '',
-        content: ''
+        text: ''
         }
     },
 
     methods: {
         save () {
-            // TODO dispatch action 
+            this.$store.dispatch('createThread', {
+                forumId: this.forum['.key'],
+                title: this.title,
+                text: this.text
+            })
         }
     }
 }
