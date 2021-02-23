63621301ad09a4335057733f731440074b52c85e
Dillon Parchment <dillonparchmentkool@gmail.com>
2021-02-22 21:55:47 -0500

    Implemented Vue.js Custom Prop Validation on Posts Edting

--- src/components/PostEditor.vue
+++ src/components/PostEditor.vue
@@ -23,7 +23,19 @@ export default {
       required: false
     },
     post: {
-      type: Object
+      type: Object,
+      validator: obj => {
+        const keyIsValid = typeof obj['.key'] === 'string'
+        const textIsValid = typeof obj.text === 'string'
+        const valid = keyIsValid && textIsValid
+        if (!textIsValid) {
+          console.log('The Post object must include a "Text" attribute')
+        }
+        if (!keyIsValid) {
+          console.log('The Post object must include a ".key" attribute')
+        }
+        return valid
+      }
     }
   },
 
